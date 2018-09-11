a1699f991142778f0d1d747d84fb03ee552eaf78
alimasroori <ali_masroori@yahoo.com>
2018-09-11 16:09:25 +0430

    s2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vf7-e6ypf3eh1kuvizql9+rv*-a53ty#-e1nzs^ul48b_))qn_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'alimasroor.pythonanywhere.com']
 
 
 # Application definition
